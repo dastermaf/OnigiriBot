4fc3d92b4b01242966021798c519599495500c08
ochkopotamus <dasterofis@gmail.com>
2023-01-28 22:28:52 +0500

    Optimization.
    New Translates in Readme.md
    Clear database.

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import config
 from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.dispatcher import Dispatcher
-from db import Database
+
 
 
 #Configs/Head
-db = Database("database.dp")
+
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
@@ -16,6 +16,7 @@ with open("mat.txt") as input:
 		words.append("".join(word.split()))
 
 #Commands Settings/Shoulders
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
 	await message.reply(text='Здравствуйте.')
@@ -24,31 +25,18 @@ async def process_start_command(message: types.Message):
 async def process_help_command(message: types.Message):
 	await message.reply(text='Сообщение о помощи.')
 
-@dp.message_handler(commands=['mute'])
-async def mute(message: types.Message):
-	if str(message.from_user.id) == config.ADMIN_ID:
-		if not message.reply_to_message:
-			await message.reply("Чигаи. Должен быть ответом на сообщение.")
-		return
-	mute_sec = int(meaage.text[6:])
-	db.add_mute(message.reply_to_message.from_user.id, mute_sec)
-	await message_reply_to_message.reply(f"Пользователь забанен на {mute_sec} секунд")
 
 #Filter/Shoulders
+
 @dp.message_handler()
 async def filter_message(message: types.Message):
 	if message.text.lower() in words:
 		await message.delete()
 
-	if not db.examination(message.from_user.id):
-		db.add(message.from_user.id)
-	if not db.mute(message.from_user.id):
-		print ("/")
-	else:
-		await message.delete()
 
 
 #Hello/Bye/Shoulders
+
 @dp.message_handler(content_types=["new_chat_members"])
 async def new_chat(message: types.Message):
 	await message.answer("Охайоу Гозаимас!")
